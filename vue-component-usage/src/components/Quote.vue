ba8706902f3d087a939a890e4bca29e128813774
dj-dedek <dedekitsjay@gmail.com>
2020-10-28 22:59:29 +0700

    vue commit

---
+++ vue-component-usage/src/components/Quote.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="panel panel-default">
+          <div class="panel-body quote">
+            <slot></slot>
+          </div>
+      </div>
+      <!-- <div class="title">
+          <slot name="title"></slot>
+          <span style="color: #ccc"><slot name="subtitle">The Subtitle</slot></span>
+      </div>
+      <hr>
+      <div>
+          <slot></slot>
+      </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    /* div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+    h2 {
+        color: red;
+    }
+    .title {
+        font-style: italic;
+    } */
+
+    .panel-body {
+        font-family: 'Arizonia', cursive;
+        font-size: 24px;
+        color: #6e6e6e;
+    }
+    .quote {
+        cursor: pointer;
+    }
+    .quote:hover {
+        background-color: #ffe2e2;
+    }
+</style>
---
+++ vuex/src/components/AnotherCounter.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="asyncIncrement({by: 50, duration: 500})">Increment</button>
+        <button class="btn btn-primary" @click="asyncDecrement({by: 50, duration: 500})">Decrement</button>
+    </div>
+</template>
+
+<script>
+
+    // import { mapMutations } from 'vuex';
+    // export default {
+    //     // methods: {
+    //     //     increment() {
+    //     //         // this.$emit('updated', 1);
+    //     //         // this.$store.state.counter++;
+    //     //         this.$store.commit('increment');
+    //     //     },
+    //     //     decrement() {
+    //     //         // this.$emit('updated', -1);
+    //     //         // this.$store.state.counter--;
+    //     //         this.$store.commit('decrement');
+    //     //     }
+    //     // }
+    //     methods: {
+    //         ...mapMutations([
+    //             'increment',
+    //             'decrement'
+    //         ])
+    //     }
+    // }
+    import { mapActions } from 'vuex';
+    export default {
+        // methods: {
+        //     increment() {
+        //         // this.$emit('updated', 1);
+        //         // this.$store.state.counter++;
+        //         this.$store.commit('increment');
+        //     },
+        //     decrement() {
+        //         // this.$emit('updated', -1);
+        //         // this.$store.state.counter--;
+        //         this.$store.commit('decrement');
+        //     }
+        // }
+        methods: {
+            ...mapActions([
+                'asyncIncrement',
+                'asyncDecrement'
+            ])
+            // increment(by) {
+            //     this.$store.dispatch('increment', by);
+            // }
+        }
+    }
+</script>
